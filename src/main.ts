bc09f8522f2ffeab4e9b63d5687c1ac3453d1776
JoshAlexis <joshyelc@gmail.com>
2022-03-05 16:17:12 -0700

    Base setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   await app.listen(3000);
 }
 bootstrap();
